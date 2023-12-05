49c768a8324155edd86c65e2fbd964bd854c016e
alantiren <tirenalan0@gmail.com>
2023-12-05 14:47:52 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    # Your implementation here
+    pass
+
+# Test cases
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,7 @@
+0x01. Lockboxes
+Algorithm
+Python
+ By: Carrie Ybay, Software Engineer at Holberton School
+ Weight: 1
+ Project will start Dec 4, 2023 6:00 AM, must end by Dec 8, 2023 6:00 AM
+ Checker was released at Dec 5, 2023 6:00 AM
