e8f54c92bac67e3ac7a64f80fcb1b7b07c438ed4
alantiren <tirenalan0@gmail.com>
2023-12-23 20:29:34 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 '''A script for parsing HTTP request logs and computing metrics.
 '''
-import sys
+
+import re
 
 
-def extract_input(line):
-    '''Extracts information from a line of an HTTP request log.
+def extract_input(input_line):
+    '''Extracts sections of a line from an HTTP request log.
 
     Args:
-        line (str): The input line to extract information from.
+        input_line (str): The input line to extract information from.
 
     Returns:
-        tuple: A tuple containing IP address, date, status code, and file size.
+        dict: A dictionary containing information such as IP address, date,
+        request,status code, and file size.
     '''
-    try:
-        _, _, _, _, _, _, _, _, _, ip, _, date, request, status_code, file_size = line.strip().split(' ', 13)
-        return ip, date, request, status_code, int(file_size)
-    except ValueError:
-        return None
+    log_patterns = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    
+    log_format = '{}\\-{}{}{}{}\\s*'.format(*log_patterns)
+
+    match = re.fullmatch(log_format, input_line)
+
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+
+    if match is not None:
+        status_code = match.group('status_code')
+        file_size = int(match.group('file_size'))
+
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    
+    return info
 
 
 def print_statistics(total_file_size, status_codes_stats):
@@ -27,11 +49,12 @@ def print_statistics(total_file_size, status_codes_stats):
         total_file_size (int): The total file size.
         status_codes_stats (dict): A dictionary containing status codes and their counts.
     '''
-    print(f'File size: {total_file_size}')
-    for code in sorted(status_codes_stats):
-        count = status_codes_stats[code]
-        if count > 0:
-            print(f'{code}: {count}')
+    print('File size: {:d}'.format(total_file_size), flush=True)
+
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
@@ -45,12 +68,15 @@ def update_metrics(line, total_file_size, status_codes_stats):
     Returns:
         int: The new total file size.
     '''
-    log_info = extract_input(line)
-    if log_info:
-        _, _, _, status_code, file_size = log_info
+
+    line_info = extract_input(line)
+    
+    status_code = line_info.get('status_code', '0')
+
+    if status_code in status_codes_stats.keys():
         status_codes_stats[status_code] += 1
-        return total_file_size + file_size
-    return total_file_size
+    
+    return total_file_size + line_info['file_size']
 
 
 def run():
@@ -58,12 +84,30 @@ def run():
     '''
     line_num = 0
     total_file_size = 0
-    status_codes_stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
 
     try:
-        for line in sys.stdin:
-            total_file_size = update_metrics(line, total_file_size, status_codes_stats)
+        while True:
+            line = input()
+            
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            
             line_num += 1
+  
             if line_num % 10 == 0:
                 print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
