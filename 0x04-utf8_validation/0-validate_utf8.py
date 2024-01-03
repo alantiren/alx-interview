fa27d16707562b0cff26cd936ffe00f63412815a
alantiren <tirenalan0@gmail.com>
2024-01-03 16:43:20 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -32,10 +32,9 @@ def validUTF8(data):
             return False
 
         if n - i >= span and all(
-        (data[j] & 0b11000000) == 0b10000000 
-        for j in range(i + 1, i + span)
-    ):
-
+                (data[j] & 0b11000000) == 0b10000000 
+                for j in range(i + 1, i + span)
+        ):
             skip = span - 1
         else:
             return False
